592169d7959252bca516fc0271c96fac3cb98c12
Fernando Picardi <fernandopicardi@gmail.com>
2025-05-27 17:10:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <tr>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <div className="container ...">
    >       <div>
    >       <div className="space-y-6 ...">
    >         <_c>
    >         <_c>
    >         <_c className="bg-card sh...">
    >           <div ref={null} className="rounded-lg...">
    >             <_c2>
    >             <_c8>
    >               <div ref={null} className="p-6 pt-0">
    >                 <AdminSelectionViewer selectedItems={[...]} onDataChange={function onDataChange}>
    >                   <div className="space-y-4">
    >                     <div className="rounded-md...">
    >                       <_c>
    >                         <div className="relative w...">
    >                           <table ref={null} className="w-full cap...">
    >                             <_c2>
    >                               <thead ref={null} className={"[&_tr]:b..."}>
    >                                 <_c8>
    > >                                 <tr
    > >                                   ref={null}
    > >                                   className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-m..."
    > >                                 >
    >                                     <_c10>
    > >                                   {" "}
    >                                     ...
    >                             ...
    >         ...
    >
    >
    > src/components/ui/table.tsx (57:3) @ _c8
    >
    >   55 |   React.HTMLAttributes<HTMLTableRowElement>
    >   56 | >(({ className, children, ...props }, ref) => (
    > > 57 |   <tr
    >      |   ^
    >   58 |     ref={ref}
    >   59 |     className={cn(
    >   60 |       'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
    >
    > Call Stack
    > 18
    >
    > Show 14 ignore-listed frame(s)
    > tr
    > <anonymous> (0:0)
    > _c8
    > src/components/ui/table.tsx (57:3)
    > AdminSelectionViewer
    > src/components/admin/selection-viewer.tsx (133:13)
    > AdminPage
    > src/app/admin/page.tsx (323:13)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -4,13 +4,12 @@ import { cn } from '@/lib/utils';
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <div className='relative w-full overflow-auto'>
     <table
       ref={ref}
       className={cn('w-full caption-bottom text-sm', className)}
-      {...props}
-    />
+      {...props}>{children}</table>
   </div>
 ));
 Table.displayName = 'Table';
@@ -18,35 +17,33 @@ Table.displayName = 'Table';
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn('[&_tr]:border-b', className)} {...props} />
+>(({ className, children, ...props }, ref) => (
+  <thead ref={ref} className={cn('[&_tr]:border-b', className)} {...props}>{children}</thead>
 ));
 TableHeader.displayName = 'TableHeader';
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <tbody
     ref={ref}
     className={cn('[&_tr:last-child]:border-0', className)}
-    {...props}
-  />
+    {...props}>{children}</tbody>
 ));
 TableBody.displayName = 'TableBody';
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <tfoot
     ref={ref}
     className={cn(
       'border-t bg-muted/50 font-medium [&>tr]:last:border-b-0',
       className
     )}
-    {...props}
-  />
+    {...props}>{children}</tfoot>
 ));
 TableFooter.displayName = 'TableFooter';
 
@@ -60,10 +57,7 @@ const TableRow = React.forwardRef<
       'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
       className
     )}
-    {...props}
-  >
-    {children}
-  </tr>
+    {...props}>{children}</tr>
 ));
 TableRow.displayName = 'TableRow';
 
@@ -77,10 +71,7 @@ const TableHead = React.forwardRef<
       'h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0',
       className
     )}
-    {...props}
-  >
-    {children}
-  </th>
+    {...props}>{children}</th>
 ));
 TableHead.displayName = 'TableHead';
 
@@ -91,10 +82,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn('p-4 align-middle [&:has([role=checkbox])]:pr-0', className)}
-    {...props}
-  >
-    {children}
-  </td>
+    {...props}>{children}</td>
 ));
 TableCell.displayName = 'TableCell';
 
@@ -105,10 +93,7 @@ const TableCaption = React.forwardRef<
   <caption
     ref={ref}
     className={cn('mt-4 text-sm text-muted-foreground', className)}
-    {...props}
-  >
-    {children}
-  </caption>
+    {...props}>{children}</caption>
 ));
 TableCaption.displayName = 'TableCaption';
 
